c9c60e1d7a159bac7d201e648077a0bba280f802
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-07-02 11:40:58 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by KPS on 7/2/2020.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Omid Joukar");
+
+    }
+}
